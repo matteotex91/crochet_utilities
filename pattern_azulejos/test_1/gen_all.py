94be9daa48e5bd72e84e46cdf42e49574bf999cd
Tessarolo Matteo MTII-GF <Matteo.Tessarolo@mt.com>
2026-04-22 10:19:51 +0200

    Update

--- pattern_azulejos/test_1/gen_all.py
+++ pattern_azulejos/test_1/gen_all.py
@@ -2,7 +2,7 @@ import numpy as np
 from gen import gen_pattern
 
 negative = True
-pattern_size = 120
+pattern_size = 121
 
 for pic_n in range(1, 19):
     gen_pattern(pic_n, negative=negative, pattern_size=pattern_size)
